c83682eba7141fb43910e167268c65def8f0ebba
AliPort <101525919+AliPort@users.noreply.github.com>
2022-10-26 20:30:14 -0500

    added password hash and user auth

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,9 +17,21 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+    
+        const data = await response.json()
+    
+        console.log(data)
+    }
        
 
-    }
+    
 
     return (
         <main>
@@ -63,6 +75,12 @@ function LoginForm() {
             </form>
         </main>
     )
-}
+        }
 
 export default LoginForm
+
+
+  
+
+
+  
